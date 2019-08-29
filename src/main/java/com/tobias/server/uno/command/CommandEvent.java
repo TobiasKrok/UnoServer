4c67fbaeb091d88fbf24be6b2f944f73fb957681
Tobias <tobias.kristiansen@norsktitanium.com>
2019-08-29 16:23:21 +0200

    Update CommandEvent.java

--- src/main/java/com/tobias/server/uno/command/CommandEvent.java
+++ src/main/java/com/tobias/server/uno/command/CommandEvent.java
@@ -2,20 +2,20 @@ package com.tobias.server.uno.command;
 
 import com.tobias.server.uno.client.UnoClient;
 
-public class CommandEvent {
+class CommandEvent {
     private String command;
     private UnoClient unoClient;
 
-    public CommandEvent(String command, UnoClient unoClient) {
+    CommandEvent(String command, UnoClient unoClient) {
         this.command = command;
         this.unoClient = unoClient;
     }
 
-    public String getStringCommand() {
+    String getStringCommand() {
         return command;
     }
 
-    public UnoClient getUnoClient() {
+    UnoClient getUnoClient() {
         return unoClient;
     }
 }
